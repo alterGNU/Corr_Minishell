576dd40d09e2996c47ab68448efbad98858138e0
alterGNU <alterGNU@gmail.com>
2025-03-25 00:51:04 +0100

    FIX:Clean tester (use ft_printf to avoid asynchronus print, clean code and add debuggind print)

--- src/unitests/test_concatenate_contiguous_single_quotes.c
+++ src/unitests/test_concatenate_contiguous_single_quotes.c
@@ -8,9 +8,8 @@
 int	printntime(char c, int n)
 {
 	for (int i = 0 ; i < n; i++)
-		printf("%c", c);
+		ft_printf("%c", c);
 	return (n);
-	fflush(stdout);
 }
 
 int	count_char_in_str(char c, char *str)
@@ -29,68 +28,66 @@ int	count_char_in_str(char c, char *str)
 
 void print_title(const char *title)
 {
-	int print_sofar = printf("=[ %s ]", title);
+	int print_sofar = ft_printf("=[ %s ]", title);
 	printntime('=', LEN - print_sofar + 1);
-	printf("\n");
-	fflush(stdout);
+	ft_printf("\n");
 }
 
 int	test(char *str, char **tab_res)
 {
-	int	i = 0;
-	t_list	*act;
-	int print_sofar = printf("%s(<%s>)", f_name, str);
+	// Print test header
+	int print_sofar = ft_printf("%s(<%s>)", f_name, str);
 	if (str)
 	{
 		int c = count_char_in_str('\t', str);
 		if (c)
 			print_sofar+=c*4;
 	}
-	t_list	*res;
-	res = build_tok_lst_split_by_quotes(str);                                // STEP1: split_by_quote(str, "\'\"") 
-	ft_printf("\nafter split by quote=");
-	print_tok_lst(res);
-	map_tok_lst_if_node_not_quoted(&res, build_tok_lst_split_by_spaces);     // STEP2: split_by_space(str, "\t\r\s\n")
-	ft_printf("\nafter split by spaces=");
-	print_tok_lst(res);
-	map_tok_lst_if_node_not_quoted(&res, build_tok_lst_split_by_operators);  // STEP3: split_by_separator(str, "|&<>","")
-	ft_printf("\nafter split by operators=");
-	print_tok_lst(res);
-	concatenate_contiguous_single_quotes(&res);
-	ft_printf("\nafter split by contiguous single quotes=");
-	print_tok_lst(res);
+	printntime('-', LEN - print_sofar);
+	// STEP1: split_by_quote(str, "\'\"") 
 	ft_printf("\n");
-	printntime('-', LEN - print_sofar - 3);
-	fflush(stdout);
-	if (!res)
+	t_list	*tok_lst = build_tok_lst_split_by_quotes(str);
+	ft_printf("\nafter split_by_quote tok_lst=");
+	print_tok_lst(tok_lst);
+	// STEP2: split_by_space(str, "\t\r\s\n") 
+	map_tok_lst_if_node_not_quoted(&tok_lst, build_tok_lst_split_by_spaces);
+	ft_printf("\nafter split_by_spaces=");
+	print_tok_lst(tok_lst);
+  	// STEP3: split_by_separator(str, "|&<>","")
+	map_tok_lst_if_node_not_quoted(&tok_lst, build_tok_lst_split_by_operators);
+	ft_printf("\nafter split_by_operators=");
+	print_tok_lst(tok_lst);
+	// STEP4: concatenate
+	concatenate_contiguous_single_quotes(&tok_lst);
+	ft_printf("\nafter split_by_contiguous_single_quotes=");
+	print_tok_lst(tok_lst);
+	ft_printf("\n");
+
+	if (!tok_lst)
 	{
 		if (!tab_res)
-			return (printf("---\n"), printntime('-', LEN - 3), printf("> ✅\n"), 0);
-		printf("---\n%s return NULL\ntab_res=", f_name); 
-		fflush(stdout);
-		ft_print_str_array(tab_res);
-		return (printf("\n"), printntime('-', LEN - 3), printf("> ❌\n"), 1);
+			return (printntime('-', LEN - 3), ft_printf("> ✅\n"), 0);
+		return (ft_printf("tab_res=", f_name), ft_print_str_array(tab_res), ft_printf("\ntok_lst=NULL\n"), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
 	}
-	act = res;
+	int	i = 0;
+	t_list	*act = tok_lst;
 	while (tab_res[i] && act && !strcmp(tab_res[i], ((t_token *)(act->content))->str))
 	{
 		act = act->next;
 		i++;
 	}
-	printf("-----\ntab_res=");
-	fflush(stdout);
+	ft_printf("tab_res=");
 	ft_print_str_array(tab_res);
-	printf("\ntok_lst=");
-	fflush(stdout);
-	print_tok_lst(res);
-	printf("\n");
+	ft_printf("\ntok_lst=");
+	print_tok_lst(tok_lst);
+	ft_printf("\n");
 	if (!tab_res[i] && !act)
-		return (ft_lstclear(&res, free_token), printntime('-', LEN - 3), printf("> ✅\n"), 0);
+		return (ft_lstclear(&tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ✅\n"), 0);
 	if ((!tab_res[i] && act) || (tab_res[i] && !act))
-		return (ft_lstclear(&res, free_token), printntime('-', LEN - 3), printf("> ❌\n"), 1);
+		return (ft_lstclear(&tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
 	if (strcmp(tab_res[i], ((t_token *)(act->content))->str))
-		return (ft_lstclear(&res, free_token), printntime('-', LEN - 3), printf("> ❌\n"), 1);
-	return (ft_lstclear(&res, free_token), printf("> ✅\n"), 0);
+		return (ft_lstclear(&tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
+	return (ft_lstclear(&tok_lst, free_token), ft_printf("> ✅\n"), 0);
 }
 
 int main()
@@ -101,29 +98,29 @@ int main()
 	nb_err += test(NULL, NULL);
 	nb_err += test("", NULL);
 
-	print_title("SIMPLE CAS NO QUOTES");
-	char *tn0[] = {" ", "echo"," ","toto"," ","|","ls",NULL};
-	nb_err += test(" echo toto |ls", tn0);
-	char *tn1[] = {"super=echo", "&&", "$super", " ", "toto", NULL};
-	nb_err += test("super=echo&&$super toto", tn1); // --> echo toto --> toto;
+	print_title("\nA|SIMPLE CAS NO QUOTES:");
+	char *a0[] = {" ", "echo"," ","toto"," ","|","ls",NULL};
+	nb_err += test(" echo toto |ls", a0);
+	char *a1[] = {"super=echo", "&&", "$super", " ", "toto", NULL};
+	nb_err += test("super=echo&&$super toto", a1); // ⚠️  --> echo toto --> toto;
 
-	print_title("SIMPLE CASES WITH QUOTES BUT NO NEED TO CONCATENATE");
-	char *t1[] = {" ", "'ec'", " ", "'ho'"," ","toto"," ","|","ls",NULL};
-	nb_err += test(" 'ec' 'ho' toto |ls", t1);
+	print_title("\nB|SIMPLE CASES WITH QUOTES BUT NO NEED TO CONCATENATE:");
+	char *b0[] = {" ", "'ec'", " ", "'ho'"," ","toto"," ","|","ls",NULL};
+	nb_err += test(" 'ec' 'ho' toto |ls", b0);
 
-	print_title("SIMPLE CASES WITH QUOTES NEED TO CONCATENATE");
-	char *t2[] = {" ", "'echo'"," ","toto"," ","|","ls",NULL};
-	nb_err += test(" 'ec''ho' toto |ls", t2);
-	char *t3[] = {"'echo'"," ","toto"," ","|","ls",NULL};
-	nb_err += test("'ec''ho' toto |ls", t3);
-	char *t4[] = {"'echo'"," ","'toto'"," ","|","ls",NULL};
-	nb_err += test("'e''c''h'o' 't''o''t'o' |ls", t4);
+	print_title("\nC|SIMPLE CASES WITH QUOTES THAT NEED TO BE CONCATENATE:");
+	char *c0[] = {" ", "'echo'"," ","toto"," ","|","ls",NULL};
+	nb_err += test(" 'ec''ho' toto |ls", c0);
+	char *c1[] = {"'echo'"," ","toto"," ","|","ls",NULL};
+	nb_err += test("'ec''ho' toto |ls", c1);
+	char *c2[] = {"'echo'"," ","'toto'"," ","|","ls",NULL};
+	nb_err += test("'e''c''h'o' 't''o''t'o' |ls", c2);
 
-	print_title("COMPLEXE IMBRICATION CASES WITH QUOTES NEED TO CONCATENATE");
-	char *tt0[] = {"echo"," ","'o\"\"i'", NULL};
-	nb_err += test("echo 'o\"''\"i'", tt0);
-	char *tt1[] = {"echo"," ","'tototiti", NULL}; // --> exec open quotes (~ heredoc << ')
-	nb_err += test("echo 'toto''titi", tt1);
+	print_title("\nD|COMPLEXE IMBRICATION CASES WITH QUOTES NEED TO CONCATENATE:");
+	char *d0[] = {"echo"," ","'o\"\"i'", NULL};
+	nb_err += test("echo 'o\"''\"i'", d0);
+	char *d1[] = {"echo"," ","'tototiti", NULL}; // ⚠️  --> exec open quotes (~ heredoc << ')
+	nb_err += test("echo 'toto''titi", d1);
 
 	return (nb_err);
 }
