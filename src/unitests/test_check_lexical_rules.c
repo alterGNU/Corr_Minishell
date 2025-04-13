446b40b672a8ee6a606597c550021be7dee3eeae
alterGNU <alterGNU@gmail.com>
2025-04-13 15:04:07 +0200

    ADD:more tests for check_lexical_rule with new unitests format(new fun, symbols etc...)

--- src/unitests/test_check_lexical_rules.c
+++ src/unitests/test_check_lexical_rules.c
@@ -1,17 +1,54 @@
-#include "minishell.h"  // build_tok_lst_split_by_quotes build_tok_lst_split_by_spaces build_tok_lst_split_by_operators set_tok_lst_type
-#include <string.h>     // strncmp
-#include <stdio.h>      // printf
-
+// =[ INCLUDE ]=================================================================
+#include "minishell.h"    // ft_printf,build_tok_lst_split_by_quotes,build_tok_lst_split_by_spaces,build_tok_lst_split_by_operators,set_tok_lst_type
+#include <string.h>       // strcmp
+// =[ DEFINE ]==================================================================
 #define LEN 90
 #define f_name "check_lexical_rules"
-
+#define CE "\033[0m"      // COLOR END
+#define CR "\033[0;31m"   // COLOR RED
+#define CV "\033[0;32m"   // COLOR GREEN
+#define CM "\033[0;33m"   // COLOR BROWN
+#define CY "\033[0;93m"   // COLOR YELLOW
+#define CB "\033[0;36m"   // COLOR AZURE
+#define CG "\033[0;37m"   // COLOR GREY
+#define CT "\033[97;100m" // COLOR GREY
+#define PASS "> \033[37;42m ✓ \033[0m\n"
+#define	FAIL "> \033[30;41m ✗ \033[0m\n"
+#define	SEPT CT"="CE
+#define	SEPSUBT CB"-"CE
+// =[ UTILS FUN ]===============================================================
+// -[ PRINTNTIME ]--------------------------------------------------------------
 int	printntime(char c, int n)
 {
 	for (int i = 0 ; i < n; i++)
 		ft_printf("%c", c);
 	return (n);
 }
-
+// -[ PRINT_TITLE ]-------------------------------------------------------------
+void print_title(const char *title)
+{
+	ft_printf("\n"CT);
+	int print_sofar = ft_printf("=[ %s ]", title);
+	printntime('=', LEN - print_sofar);
+	ft_printf(CE"\n");
+}
+// -[ PRINT_SUB_TITLE ]---------------------------------------------------------
+void print_subtitle(const char *title)
+{
+	ft_printf("\n"CB);
+	int print_sofar = ft_printf("- %s ", title);
+	printntime('-', LEN - print_sofar);
+	ft_printf(CE"\n");
+}
+// -[ PRINT_SEP ]---------------------------------------------------------------
+void print_sep(const char *sep)
+{
+	int	i = -1;
+	while (++i < LEN)
+		ft_printf("%s", sep);
+	ft_printf("\n");
+}
+// -[ COUNT_CHAR_IN_STR ]-------------------------------------------------------
 int	count_char_in_str(char c, char *str)
 {
 	int	res;
@@ -25,18 +62,12 @@ int	count_char_in_str(char c, char *str)
 	}
 	return (res);
 }
-
-void print_title(const char *title)
-{
-	int print_sofar = ft_printf("\n=[ %s ]", title);
-	printntime('=', LEN - print_sofar + 2);
-	ft_printf("\n");
-}
-
+// =[ TESTS FUNCTIONS ]=========================================================
+// This function print details only on failures.
 int	test(t_data *dt, char *str, char **tab_res, int *type_res)
 {
 	// Print test header
-	int print_sofar = ft_printf("%s(<%s>)", f_name, str);
+	int print_sofar = ft_printf("%s(\"%s\")", f_name, str);
 	if (str)
 	{
 		int c = count_char_in_str('\t', str);
@@ -47,57 +78,56 @@ int	test(t_data *dt, char *str, char **tab_res, int *type_res)
 	// STEP1: split_by_quote(str, "\'\"") 
 	ft_printf("\n");
 	dt->tok_lst = build_tok_lst_split_by_quotes(str);
-	ft_printf("\nafter split_by_quote tok_lst=");
-	print_tok_lst(dt->tok_lst);
+	//ft_printf("\nafter split_by_quote tok_lst=");
+	//print_tok_lst(dt->tok_lst);
 	// STEP2: split_by_space(str, "\t\r\s\n") 
 	map_tok_lst_if_node_not_quoted(&dt->tok_lst, build_tok_lst_split_by_spaces);
-	ft_printf("\nafter split_by_spaces=");
-	print_tok_lst(dt->tok_lst);
+	//ft_printf("\nafter split_by_spaces=");
+	//print_tok_lst(dt->tok_lst);
  	// STEP3: split_by_separator(str, "|&<>","")
 	map_tok_lst_if_node_not_quoted(&dt->tok_lst, build_tok_lst_split_by_operators);
-	ft_printf("\nafter split_by_operators=");
-	print_tok_lst(dt->tok_lst);
-	// STEP4: concatenate
+	//ft_printf("\nafter split_by_operators=");
+	//print_tok_lst(dt->tok_lst);
+ 	// STEP4: split_by_separator(str, "|&<>","")
+	map_tok_lst_if_node_not_quoted(&dt->tok_lst, build_tok_lst_split_by_parenthesis);
+	//ft_printf("\nafter split_by_parenthesis=");
+	//print_tok_lst(dt->tok_lst);
+	// STEP5: concatenate
 	concatenate_contiguous_str(&dt->tok_lst);
-	ft_printf("\nafter split_by_contiguous_single_quotes=");
-	print_tok_lst(dt->tok_lst);
-	// STEP 5: set_tok_lst_type
+	//ft_printf("\nafter split_by_contiguous_single_quotes=");
+	//print_tok_lst(dt->tok_lst);
+	// STEP 6: set_tok_lst_type
 	set_tok_lst_type(dt->tok_lst);
-	ft_printf("\nafter set_tok_lst_type=");
-	print_tok_lst(dt->tok_lst);
-	// STEP 6: check_lexical_rules
+	//ft_printf("\nafter set_tok_lst_type=");
+	//print_tok_lst(dt->tok_lst);
+	// STEP 7: check_lexical_rules
 	check_lexical_rules(dt);
-	ft_printf("\nafter check_lexical_rules:[%d]=", dt->last_cmd_exit_status);
-	print_tok_lst(dt->tok_lst);
+	//ft_printf("\nafter check_lexical_rules:[%d]=", dt->last_cmd_exit_status);
+	//print_tok_lst(dt->tok_lst);
 	ft_printf("\n");
 
 	if (!dt->tok_lst)
 	{
 		if (!tab_res)
-			return (printntime('-', LEN - 3), ft_printf("> ✅\n"), 0);
-		return (ft_printf("tab_res=", f_name), ft_print_str_array(tab_res), ft_printf("\ntok_lst=NULL\n"), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
+			return (printntime('-', LEN - 5), ft_printf(PASS), 0);
+		return (ft_printf("tab_res=", f_name), ft_print_str_array(tab_res), ft_printf("\ntok_lst=NULL\n"), printntime('-', LEN - 5), ft_printf(FAIL), 1);
 	}
 	int	i = 0;
 	t_list	*act = dt->tok_lst;
 	while (tab_res[i] && act && !strcmp(tab_res[i], ((t_token *)(act->content))->str))
 	{
 		if (type_res[i] != ((t_token *)act->content)->type)
-			return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
+			return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 5), ft_printf(FAIL), 1);
 		act = act->next;
 		i++;
 	}
-	ft_printf("tab_res=");
-	ft_print_str_array(tab_res);
-	ft_printf("\ntok_lst=");
-	print_tok_lst(dt->tok_lst);
-	ft_printf("\n");
 	if (!tab_res[i] && !act)
-		return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ✅\n"), 0);
+		return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 5), ft_printf(PASS), 0);
 	if ((!tab_res[i] && act) || (tab_res[i] && !act))
-		return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
+		return (ft_printf("tab_res="), ft_print_str_array(tab_res), ft_printf("\ntok_lst="), print_tok_lst(dt->tok_lst), ft_printf("\n"),ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 5), ft_printf(FAIL), 1);
 	if (strcmp(tab_res[i], ((t_token *)(act->content))->str))
-		return (ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 3), ft_printf("> ❌\n"), 1);
-	return (ft_lstclear(&dt->tok_lst, free_token), ft_printf("> ✅\n"), 0);
+		return (ft_printf("tab_res="), ft_print_str_array(tab_res), ft_printf("\ntok_lst="), print_tok_lst(dt->tok_lst), ft_printf("\n"),ft_lstclear(&dt->tok_lst, free_token), printntime('-', LEN - 5), ft_printf(FAIL), 1);
+	return (ft_lstclear(&dt->tok_lst, free_token), ft_printf(PASS), 0);
 }
 
 int main(int ac, char **av, char **ev)
@@ -111,14 +141,25 @@ int main(int ac, char **av, char **ev)
 	int	nb_err = 0;
 
 	print_title("CAS:NULL");
+	print_subtitle("These cases never really happen because they're handled in main, before lexer(dt, line) is called");
 	nb_err += test(dt, NULL, NULL, NULL);
 	nb_err += test(dt, "", NULL, NULL);
+	print_sep(SEPSUBT);
 
 	print_title("A| PASS COMMANDS");
+	print_subtitle("Only unset and spaces-> check if quotes is count as one UNSET");
+	int ai0[9] = {ESP, UNSET, ESP, UNSET, ESP, UNSET, ESP, UNSET, ESP};
+	char *as0[] = {" ", "cmd1", " ", "arg1", " ", "arg2", " ", "\"arg3 && arg4\"", " ", NULL};
+	nb_err += test(dt, " cmd1 arg1   arg2   \"arg3 && arg4\" ", as0, ai0);
+	print_sep(SEPSUBT);
+
+	print_subtitle("Real command with REDIR and PIPE OPERATORS");
 	int ai1[13] = {RLS, ESP, UNSET, ESP, UNSET, ESP, PIP, ESP, UNSET, ESP, RRS, ESP, UNSET};
 	char *as1[] = {"<", " ", "file1", " ", "cat", " ", "|", " ", "cat", " ", ">", " ", "file2", NULL};
 	nb_err += test(dt, "< file1 cat | cat > file2", as1, ai1);
+	print_sep(SEPSUBT);
 	
+	print_subtitle("3 tests to check concatenation of contiguous quoted unset");
 	int ai2[5] = {UNSET, ESP, UNSET, RRD, UNSET};
 	char *as2[] = {"'e'''cho", " ", "toto", ">>", "file1", NULL};
 	nb_err += test(dt, "'e'''cho toto>>file1", as2, ai2);
@@ -130,69 +171,180 @@ int main(int ac, char **av, char **ev)
 	int ai4[13] = {ESP, UNSET, ESP, UNSET, OPA, UNSET, ESP, UNSET, OPO, UNSET, ESP, UNSET, ESP};
 	char *as4[] = {" ","'e'\"c\"ho", " ", "toto", "&&", "echo", " ", "OK", "||", "echo"," ", "KO", " ", NULL};
 	nb_err += test(dt, "    'e'\"c\"ho  toto&&echo OK||echo  KO    ", as4, ai4);
+	print_sep(SEPSUBT);
 
+	print_subtitle("Check all binary operators");
 	int ai5[8] = {UNSET, PIP, UNSET, OPA, UNSET, OPO, UNSET};
 	char *as5[] = {"cmd1", "|", "cmd2", "&&", "cmd3", "||", "cmd4", NULL};
 	nb_err += test(dt, "cmd1|cmd2&&cmd3||cmd4", as5, ai5);
+
 	int ai6[15] = {ESP, UNSET, ESP, PIP, ESP, UNSET, ESP, OPA, ESP, UNSET, ESP, OPO, ESP, UNSET, ESP};
 	char *as6[] = {" ", "cmd1", " ",  "|", " ",  "cmd2", " ",  "&&", " ",  "cmd3", " ",  "||", " ",  "cmd4", " ",  NULL};
 	nb_err += test(dt, "   cmd1   |   cmd2   &&   cmd3   ||   cmd4   ", as6, ai6);
+	print_sep(SEPSUBT);
 
-	print_title("B| FAIL MULTIPLES");
-	nb_err += test(dt, "< << <<< <<<< <<<<< <<<<<<   ", NULL, NULL);
-	nb_err += test(dt, "< << <<< <<<< <<<<< <<<<<<", NULL, NULL);
-	nb_err += test(dt, "> >> >>> >>>> >>>>> >>>>>>", NULL, NULL);
-	nb_err += test(dt, "| || ||| |||| ||||| ||||||", NULL, NULL);
-	nb_err += test(dt, "& && &&& &&&& &&&&& &&&&&&", NULL, NULL);
-	nb_err += test(dt, " <><<<< < ||| & &&&", NULL, NULL);
-	nb_err += test(dt, " <><<<< < ||| & &&&", NULL, NULL);
+	print_subtitle("Check all unary operators");
+	int ai7[10] = {RLS, UNSET, RLD, UNSET, RLT, UNSET, RRS, UNSET, RRD, UNSET};
+	char *as7[] = {"<", "f1", "<<", "f2", "<<<", "f3", ">", "f4",">>", "f5", NULL};
+	nb_err += test(dt, "<f1<<f2<<<f3>f4>>f5", as7, ai7);
 
-	print_title("C| FAIL <");
-	nb_err += test(dt, "cat < |", NULL, NULL);
-	nb_err += test(dt, "cat < ", NULL, NULL);
-	nb_err += test(dt, "cat <", NULL, NULL);
+	int ai8[21] = {ESP, RLS, ESP, UNSET, ESP, RLD, ESP, UNSET, ESP, RLT, ESP, UNSET, ESP, RRS, ESP, UNSET, ESP, RRD, ESP, UNSET, ESP};
+	char *as8[] = {" ", "<", " ", "f1", " ", "<<", " ", "f2", " ", "<<<", " ", "f3", " ", ">", " ", "f4", " ",">>", " ", "f5", " ", NULL};
+	nb_err += test(dt, "  <  f1  <<  f2  <<<  f3  >  f4  >>  f5  ", as8, ai8);
+	print_sep(SEPSUBT);
 
-	print_title("D| FAIL <<");
-	nb_err += test(dt, "cat << |", NULL, NULL);
-	nb_err += test(dt, "cat << ", NULL, NULL);
-	nb_err += test(dt, "cat <<", NULL, NULL);
+	print_subtitle("Check parenthesis");
+	int ai9[3] = {PARO, UNSET, PARC};
+	char *as9[] = {"(", "cmd", ")", NULL};
+	nb_err += test(dt, "(cmd)", as9, ai9);
 
-	print_title("E| FAIL <<<");
-	nb_err += test(dt, "<<< |", NULL, NULL);
-	nb_err += test(dt, "<<< ", NULL, NULL);
-	nb_err += test(dt, "<<<", NULL, NULL);
+	int ai10[11] = {PARO, UNSET, OPA, UNSET, PARC, OPO, PARO, UNSET, OPA, UNSET, PARC};
+	char *as10[] = {"(", "cmd1", "&&", "cmd2", ")", "||", "(", "cmd3", "&&", "cmd4", ")", NULL};
+	nb_err += test(dt, "(cmd1&&cmd2)||(cmd3&&cmd4)", as10, ai10);
 
-	print_title("E| FAIL >");
-	nb_err += test(dt, "echo toto >|", NULL, NULL);
-	nb_err += test(dt, "echo toto >", NULL, NULL);
-	nb_err += test(dt, "echo toto > ", NULL, NULL);
+	int ai11[10] = {PARO, PARO, PARO, UNSET, OPA, UNSET, PARC, PARC, PARC};
+	char *as11[] = {"(", "(", "(", "cmd1", "&&", "cmd2", ")", ")", ")",NULL};
+	nb_err += test(dt, "(((cmd1&&cmd2)))", as11, ai11);
 
-	print_title("G| FAIL >>");
-	nb_err += test(dt, "echo toto >>|", NULL, NULL);
-	nb_err += test(dt, "echo toto >>", NULL, NULL);
-	nb_err += test(dt, "echo toto >> ", NULL, NULL);
+	int ai12[16] = {ESP, PARO, ESP, PARO, ESP, PARO, UNSET, OPA, UNSET, PARC, ESP, PARC, ESP, PARC, ESP};
+	char *as12[] = {" ", "(", " ", "(", " ", "(", "cmd1", "&&", "cmd2", ")", " ", ")", " ", ")", " ", NULL};
+	nb_err += test(dt, " ( ( (cmd1&&cmd2) ) ) ", as12, ai12);
 
-	print_title("H| FAIL &&");
-	nb_err += test(dt, "&&", NULL, NULL);
-	nb_err += test(dt, "echo toto&&", NULL, NULL);
-	nb_err += test(dt, "&&echo toto", NULL, NULL);
+	int ai13[9] = {UNSET, RLS, UNSET, PIP, RRS, UNSET, ESP, UNSET};
+	char *as13[] = {"cmd1", "<", "f2", "|", ">", "f3", " ", "cmd2", NULL};
+	nb_err += test(dt, "cmd1<f2|>f3 cmd2", as13, ai13);
+	print_sep(SEPSUBT);
+	print_sep(SEPT);
 
-	print_title("I| FAIL ||");
-	nb_err += test(dt, "||", NULL, NULL);
-	nb_err += test(dt, "echo toto||", NULL, NULL);
-	nb_err += test(dt, "||echo toto", NULL, NULL);
-
-	print_title("J| FAIL |");
-	nb_err += test(dt, "|", NULL, NULL);
-	nb_err += test(dt, "ls|", NULL, NULL);
-	nb_err += test(dt, "|ls", NULL, NULL);
-
-	print_title("K| FAIL ERR_TYPE");
+	print_title("B| FAIL UNARY UNKNOWN OPERATOR");
 	nb_err += test(dt, "<<<<", NULL, NULL);
+	nb_err += test(dt, "<<<<f1", NULL, NULL);
 	nb_err += test(dt, ">>>", NULL, NULL);
+	nb_err += test(dt, ">>>f1", NULL, NULL);
 	nb_err += test(dt, "|||", NULL, NULL);
+	nb_err += test(dt, "cmd1|||cmd2", NULL, NULL);
 	nb_err += test(dt, "&&&", NULL, NULL);
-	nb_err += test(dt, "&", NULL, NULL);
-	nb_err += test(dt, "&&&&", NULL, NULL);
+	nb_err += test(dt, "cmd1&&&cmd2", NULL, NULL);
+	print_subtitle("List of operators not handle for now");
+	nb_err += test(dt, "<(cmd)", NULL, NULL);
+	nb_err += test(dt, ">(cmd)", NULL, NULL);
+	print_sep(SEPSUBT);
+	print_sep(SEPT);
+
+	print_title("C| FAIL UNARY RLS <");
+	nb_err += test(dt, "<", NULL, NULL);
+	nb_err += test(dt, "< ", NULL, NULL);
+	nb_err += test(dt, "<>f1", NULL, NULL);
+	nb_err += test(dt, " < > f1 ", NULL, NULL);
+	nb_err += test(dt, "cmd1<", NULL, NULL);
+	nb_err += test(dt, "<|cmd2", NULL, NULL);
+	nb_err += test(dt, "<&&cmd2", NULL, NULL);
+	nb_err += test(dt, "<||cmd2", NULL, NULL);
+	nb_err += test(dt, "< (cmd1&&cmd2)", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("D| FAIL UNARY RLD <<");
+	nb_err += test(dt, "<<", NULL, NULL);
+	nb_err += test(dt, "<< ", NULL, NULL);
+	nb_err += test(dt, "<<>f1", NULL, NULL);
+	nb_err += test(dt, " << > f1 ", NULL, NULL);
+	nb_err += test(dt, "cmd1<<", NULL, NULL);
+	nb_err += test(dt, "<<|cmd2", NULL, NULL);
+	nb_err += test(dt, "<<&&cmd2", NULL, NULL);
+	nb_err += test(dt, "<<||cmd2", NULL, NULL);
+	nb_err += test(dt, "<< (cmd1&&cmd2)", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("E| FAIL UNARY RLT <<<");
+	nb_err += test(dt, "<<<", NULL, NULL);
+	nb_err += test(dt, "<<< ", NULL, NULL);
+	nb_err += test(dt, "<<<>f1", NULL, NULL);
+	nb_err += test(dt, " <<< > f1 ", NULL, NULL);
+	nb_err += test(dt, "cmd1<<<", NULL, NULL);
+	nb_err += test(dt, "<<<|cmd2", NULL, NULL);
+	nb_err += test(dt, "<<<&&cmd2", NULL, NULL);
+	nb_err += test(dt, "<<<||cmd2", NULL, NULL);
+	nb_err += test(dt, "<<< (cmd1&&cmd2)", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("F| FAIL UNARY RRS >");
+	nb_err += test(dt, ">", NULL, NULL);
+	nb_err += test(dt, "> ", NULL, NULL);
+	nb_err += test(dt, "cmd>", NULL, NULL);
+	nb_err += test(dt, "cmd> ", NULL, NULL);
+	nb_err += test(dt, "<f1>", NULL, NULL);
+	nb_err += test(dt, " < f1 > ", NULL, NULL);
+	nb_err += test(dt, ">|cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>|cmd2", NULL, NULL);
+	nb_err += test(dt, ">||cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>||cmd2", NULL, NULL);
+	nb_err += test(dt, ">&&cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>&&cmd2", NULL, NULL);
+	nb_err += test(dt, "> (cmd1&&cmd2)", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("G| FAIL UNARY RRD >");
+	nb_err += test(dt, ">>", NULL, NULL);
+	nb_err += test(dt, ">> ", NULL, NULL);
+	nb_err += test(dt, "cmd>>", NULL, NULL);
+	nb_err += test(dt, "cmd>> ", NULL, NULL);
+	nb_err += test(dt, "<f1>>", NULL, NULL);
+	nb_err += test(dt, " < f1 >> ", NULL, NULL);
+	nb_err += test(dt, ">>|cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>>|cmd2", NULL, NULL);
+	nb_err += test(dt, ">>||cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>>||cmd2", NULL, NULL);
+	nb_err += test(dt, ">>&&cmd2", NULL, NULL);
+	nb_err += test(dt, "cmd1>>&&cmd2", NULL, NULL);
+	nb_err += test(dt, ">> (cmd1&&cmd2)", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("H| FAIL BINARY OPA &&");
+	nb_err += test(dt, "&&", NULL, NULL);
+	nb_err += test(dt, " && ", NULL, NULL);
+	nb_err += test(dt, "cmd&&", NULL, NULL);
+	nb_err += test(dt, " cmd && ", NULL, NULL);
+	nb_err += test(dt, "&&cmd", NULL, NULL);
+	nb_err += test(dt, " && cmd ", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("I| FAIL BINARY OPO ||");
+	nb_err += test(dt, "||", NULL, NULL);
+	nb_err += test(dt, " || ", NULL, NULL);
+	nb_err += test(dt, "cmd||", NULL, NULL);
+	nb_err += test(dt, " cmd || ", NULL, NULL);
+	nb_err += test(dt, "||cmd", NULL, NULL);
+	nb_err += test(dt, " || cmd ", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("J| FAIL BINARY PIP |");
+	nb_err += test(dt, "|", NULL, NULL);
+	nb_err += test(dt, " | ", NULL, NULL);
+	nb_err += test(dt, "cmd|", NULL, NULL);
+	nb_err += test(dt, " cmd | ", NULL, NULL);
+	nb_err += test(dt, "|cmd", NULL, NULL);
+	nb_err += test(dt, " | cmd ", NULL, NULL);
+	print_sep(SEPT);
+
+	print_title("K| FAIL WITH PARENTHESIS");
+	print_subtitle("Cases of 'Empty parenthesis'");
+	nb_err += test(dt, "()", NULL, NULL);
+	nb_err += test(dt, " ( ) ", NULL, NULL);
+	nb_err += test(dt, "(())", NULL, NULL);
+	nb_err += test(dt, " ( ( ) ) ", NULL, NULL);
+	print_sep(SEPSUBT);
+
+	print_subtitle("Cases of 'Odd number of parenthesis'");
+	nb_err += test(dt, "(", NULL, NULL);
+	nb_err += test(dt, " ( ", NULL, NULL);
+	//nb_err += test(dt, ")", NULL, NULL);           //#TODO:🎯FIX-->SEGFAULT
+	//nb_err += test(dt, " ) ", NULL, NULL);         //#TODO:🎯FIX-->SEGFAULT
+	//nb_err += test(dt, "((cmd)", NULL, NULL);      //#TODO:🎯FIX-->SEGFAULT
+	//nb_err += test(dt, " ( ( cmd ) ", NULL, NULL); //#TODO:🎯FIX-->SEGFAULT
+	//nb_err += test(dt, "(cmd))", NULL, NULL);      //#TODO:🎯FIX-->SEGFAULT
+	//nb_err += test(dt, " ( cmd ) )", NULL, NULL);  //#TODO:🎯FIX-->SEGFAULT
+	print_sep(SEPSUBT);
+	print_sep(SEPT);
+
 	return (free_data(&dt), nb_err);
 }
