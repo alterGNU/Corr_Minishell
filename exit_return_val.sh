d1c77eadc109a89255ea6669df759a5be332ff0b
alterGNU <alterGNU@gmail.com>
2025-06-30 14:25:18 +0200

    FIX:always print

---
+++ exit_return_val.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bash_rv=$(echo "exit $1" | bash --posix > /dev/null 2>&1 && echo $? || echo $?)
+mshl_rv=$(echo "exit $1" | ./minishell > /dev/null 2>&1 && echo $? || echo $?)
+echo "bash_rv:${bash_rv} VS mshl_rv:${mshl_rv}"
+[[ "${bash_rv}" -eq "${mshl_rv}" ]] && echo "✅PASS" || echo "❌FAIL"
