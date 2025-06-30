3413987994eb99a7ec59e65591e47d95966ec236
alterGNU <alterGNU@gmail.com>
2025-06-30 14:32:30 +0200

    FIX:always print error message too

--- exit_return_val.sh
+++ exit_return_val.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 bash_rv=$(echo "exit $1" | bash --posix > /dev/null 2>&1 && echo $? || echo $?)
+echo "exit $1" | bash --posix
 mshl_rv=$(echo "exit $1" | ./minishell > /dev/null 2>&1 && echo $? || echo $?)
+echo "exit $1" | ./minishell
 echo "bash_rv:${bash_rv} VS mshl_rv:${mshl_rv}"
 [[ "${bash_rv}" -eq "${mshl_rv}" ]] && echo "✅PASS" || echo "❌FAIL"
